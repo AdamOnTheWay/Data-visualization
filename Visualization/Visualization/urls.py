b5872acbc8e79703e25f741f1645e25cb9c81186
AdamOnTheWay <cydcoder@gmail.com>
2017-03-27 17:58:27 +0800

    test views and statis

--- Visualization/Visualization/urls.py
+++ Visualization/Visualization/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from visualApp import views as visualAppViews
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^hello/', visualAppViews.Hello_world_test),
 ]
--- Visualization/visualApp/views.py
+++ Visualization/visualApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+
+def Hello_world_test(request):
+	#return HttpResponse("Hello,world")
+	return render(request,"hello.html")

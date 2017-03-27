af6d14edb9e84e5ef74651a4e6cb494204596674
AdamOnTheWay <cydcoder@gmail.com>
2017-03-27 17:13:25 +0800

    setting templates

--- Visualization/Visualization/settings.py
+++ Visualization/Visualization/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Visualization.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,3 +117,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_URL = '/static/'
+
